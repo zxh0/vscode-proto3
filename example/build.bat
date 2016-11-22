0d98a3735ecc28351030a58142b13a3f43bab5a8
zxh0 <zhangxiuhong2013@gmail.com>
2016-11-22 19:22:33 +0800

    fix build.bat

--- example/build.bat
+++ example/build.bat
@@ -10,7 +10,7 @@ md %JAVA_OUT%
   --proto_path=protos\v3^
   --proto_path=protos/v2^
   --java_out=%JAVA_OUT%^
-  protos\v3\foo\bar\msg.proto^
-  protos\v3\*.proto^
-  protos\v2\foo\bar\msg.proto^
-  protos\v2\*.proto
+  protos/v3/foo/bar/msg.proto^
+  protos/v2/foo/bar/msg_v2.proto^
+  protos/v3/*.proto^
+  protos/v2/*.proto
