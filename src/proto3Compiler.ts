cb8c4515375152c5f2c7180b76fc7cfa9471a202
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-12-07 15:52:05 +0900

    Workaround for ${} key word in config variables.

--- src/proto3Compiler.ts
+++ src/proto3Compiler.ts
@@ -9,9 +9,11 @@ import os = require('os');
 export class Proto3Compiler {
 
     private _settings: vscode.WorkspaceConfiguration;
+    private _configResolver: ConfigurationResolver;
 
     constructor() {
-        this._settings = vscode.workspace.getConfiguration("protoc")
+        this._settings = vscode.workspace.getConfiguration("protoc");
+        this._configResolver = new ConfigurationResolver();
     }
 
     public compileAllProtos() {
@@ -59,11 +61,13 @@ export class Proto3Compiler {
     }
 
     private getProtocPath(): string {
-        return this._settings.get<string>('path', 'protoc');
+        return this._configResolver.resolve(
+            this._settings.get<string>('path', 'protoc'));
     }
 
     private getProtocOptions(): string[] {
-        return this._settings.get<string[]>('options', []);
+        return this._configResolver.resolve(
+            this._settings.get<string[]>('options', []));
     }
 
     private getProtoPathOptions(): string[] {
@@ -100,4 +104,83 @@ class InputCollector {
         return this.inputs;
     }
 
+}
+
+// Workaround to substitute variable keywords in the configuration value until
+// workbench/services/configurationResolver is available on Extention API.
+//
+// 
+// Some codes are copied from:
+// src/vs/workbench/services/configurationResolver/node/configurationResolverService.ts
+class ConfigurationResolver {
+
+    constructor(){
+        Object.keys(process.env).forEach(key => {
+			this[`env.${key}`] = process.env[key];
+		});
+    }
+
+	public resolve(value: string): string;
+	public resolve(value: string[]): string[];
+    public resolve(value: any): any {
+        if (typeof value === 'string') {
+            return this.resolveString(value);
+        } else if (typeof value === 'array' || typeof value === 'object') {
+            return this.resolveArray(value);
+        }
+        return value;
+    }
+
+	private resolveArray(value: string[]): string[] {
+		return value.map(s => this.resolveString(s));
+	}
+
+    private resolveString(value: string): string {
+		let regexp = /\$\{(.*?)\}/g;
+		const originalValue = value;
+		const resolvedString = value.replace(regexp, (match: string, name: string) => {
+			let newValue = (<any>this)[name];
+			if (typeof newValue === 'string') {
+				return newValue;
+			} else {
+				return match && match.indexOf('env.') > 0 ? '' : match;
+			}
+		});
+
+		return this.resolveConfigVariable(resolvedString, originalValue);
+	}
+    
+    private resolveConfigVariable(value: string, originalValue: string): string {
+		let regexp = /\$\{config\.(.+?)\}/g;
+		return value.replace(regexp, (match: string, name: string) => {
+			let config = vscode.workspace.getConfiguration();
+			let newValue: any;
+			try {
+				const keys: string[] = name.split('.');
+				if (!keys || keys.length <= 0) {
+					return '';
+				}
+				while (keys.length > 1) {
+					const key = keys.shift();
+					if (!config || !config.hasOwnProperty(key)) {
+						return '';
+					}
+					config = config[key];
+				}
+				newValue = config && config.hasOwnProperty(keys[0]) ? config[keys[0]] : '';
+			} catch (e) {
+				return '';
+			}
+			if (typeof newValue === 'string') {
+				// Prevent infinite recursion and also support nested references (or tokens)
+				return newValue === originalValue ? '' : this.resolveString(newValue);
+			} else {
+				return this.resolve(newValue) + '';
+			}
+		});
+	}
+
+    private get workspaceRoot(): string {
+		return vscode.workspace.rootPath;
+	}
 }
