53b39d7facc29a41ac32f10794f106a7ea3d9c30
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-01-08 09:41:49 +0900

    Fix the range calculation for the error diagnostics.

--- src/proto3Diagnostic.ts
+++ src/proto3Diagnostic.ts
@@ -8,37 +8,53 @@ export class Proto3LanguageDiagnosticProvider {
 
     private errors = vscode.languages.createDiagnosticCollection("languageErrors");
 
-    public createDiagnostics(docUri: vscode.Uri, fileName: string) {
-        new Proto3Compiler().compileProtoToTmp(fileName, stderr => {
-            //console.log(stderr);
+    public createDiagnostics(doc: vscode.TextDocument) {
+        new Proto3Compiler().compileProtoToTmp(doc.fileName, stderr => {
             if (stderr) {
-                this.analyzeErrors(docUri, fileName, stderr);
+                this.analyzeErrors(stderr, doc);
             } else {
-                this.errors.delete(docUri);
+                this.errors.delete(doc.uri);
             }
         })
     }
     
-    private analyzeErrors(docUrl: vscode.Uri, fileName: string, stderr: string) {
-        let shortFileName = path.parse(fileName).name;
+    private analyzeErrors(stderr: string, doc: vscode.TextDocument) {
+        let shortFileName = path.parse(doc.fileName).name;
         let diagnostics = stderr.split('\n')
             .filter(line => line.includes(shortFileName))
-            .map(line => this.parseErrorLine(line))
+            .map(line => this.parseErrorLine(line, doc))
             .filter(diagnostic => diagnostic != null);
 
-        this.errors.set(docUrl, diagnostics);
+        this.errors.set(doc.uri, diagnostics);
     }
 
-    private parseErrorLine(line: string): vscode.Diagnostic {
-        let errorInfo = line.match(/\w+\.proto:(\d+):(\d+):\s*(.*)/);
-        if (errorInfo) {
-            let startLine = parseInt(errorInfo[1]) - 1;
-            let startChar = parseInt(errorInfo[2]) - 1;
-            let range = new vscode.Range(startLine, startChar, startLine, line.length);
-            let msg = errorInfo[3];
-            return new vscode.Diagnostic(range, msg, vscode.DiagnosticSeverity.Error);
+    private parseErrorLine(errline: string, doc: vscode.TextDocument): vscode.Diagnostic {
+        let errorInfo = errline.match(/\w+\.proto:(\d+):(\d+):\s*(.*)/);
+        if (!errorInfo) {
+            return null;
         }
-        return null;
+        let startLine = parseInt(errorInfo[1]) - 1;
+        let startCol = parseInt(errorInfo[2]) - 1;
+        
+        // protoc calculates tab width (eight spaces) and returns colunm number.
+        let line = doc.lineAt(startLine);
+        let startChar = 0;
+        let col = 0;
+        for(var c of line.text) {
+            col += ( c === "\t" ? (8 - col % 8): 1 );
+            startChar += 1;
+            if( col >= startCol ) {
+                break;
+            }
+        }
+        let endChar = line.text.length;
+        let tokenEnd = line.text.substr(startChar).match(/[\s;{}\[\],<>()=]/);
+        if (tokenEnd) {
+            endChar = startChar + tokenEnd.index;
+        }
+        let range = new vscode.Range(startLine, startChar, startLine, endChar);
+        let msg = errorInfo[3];
+        return new vscode.Diagnostic(range, msg, vscode.DiagnosticSeverity.Error);
     }
 
 }
